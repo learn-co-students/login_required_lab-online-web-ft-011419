58c5158d90b79cb9d0fd778fa7c9a8e66b45d6af
Jared Harbison <jared.harbison@gmail.com>
2019-04-03 13:01:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/show' => 'secrets#show'
+  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
